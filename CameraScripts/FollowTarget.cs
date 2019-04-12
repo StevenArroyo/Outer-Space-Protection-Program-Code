f30d53801e2332258a97f8e62d68cabf87f6a4af
StevenArroyo <47120054+StevenArroyo@users.noreply.github.com>
2019-04-12 13:28:37 -0400

    Add files via upload

---
+++ CameraScripts/FollowTarget.cs
@@ -0,0 +1,50 @@
+﻿// Scott Schubr January 7th 2017
+// Credits to Unity Pro Tutorials, Renaissance Coders, Brackeys, and Sharp Accent tutorials, as well as other Youtube tutorials.
+
+using UnityEngine;
+
+public abstract class FollowTarget : MonoBehaviour {
+	
+	[SerializeField] public Transform target;
+	[SerializeField] private bool autoTargetPlayer = true;
+
+	virtual protected void Start()
+	{
+		if (autoTargetPlayer)
+		{
+			FindTargetPlayer();
+			}
+		}
+
+	void FixedUpdate () 
+	{
+	  if (autoTargetPlayer && (target == null || !target.gameObject.activeSelf))
+		{
+			FindTargetPlayer();
+				}
+		if (target != null && (target.GetComponent<Rigidbody>() != null && !target.GetComponent<Rigidbody>().isKinematic)) 
+		{
+			Follow(Time.deltaTime);
+				}
+	}
+
+	protected abstract void Follow(float deltaTime);
+	
+
+	public void FindTargetPlayer()
+	{
+		if (target == null)
+		{
+			GameObject targetObj = GameObject.FindGameObjectWithTag("Player");
+				if(targetObj)
+			{
+				SetTarget(targetObj.transform);
+			}
+				}
+		}
+	public virtual void SetTarget(Transform newTransform)
+	{
+		target = newTransform;
+	}
+	public Transform Target{get {return this.target;}}
+}
---
+++ CameraScripts/ShakeCamera.cs
@@ -0,0 +1,26 @@
+﻿// Scott Schubr January 7th 2017
+// Credits to Unity Pro Tutorials, Renaissance Coders, Brackeys, and Sharp Accent tutorials, as well as other Youtube tutorials.
+
+using UnityEngine;
+using System.Collections;
+
+public class ShakeCamera : MonoBehaviour {
+
+    public float positionShakeSpeed = 0.1f;
+    public Vector3 positionShakeRange = new Vector3(0.1f, 0.1f, 0.1f);
+
+	private Vector3 position;
+
+	void Start () 
+    {
+        position = transform.localPosition;
+	}
+	
+	void Update () 
+    {
+        if (positionShakeSpeed > 0)
+        {
+            transform.localPosition = position + Vector3.Scale(SmoothRandom.GetVector3(positionShakeSpeed), positionShakeRange);
+        }
+	}
+}
